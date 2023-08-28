1791b2f97fd4630b7bc54b6055eaf838d28a9796
Snigdho Dip Howlader <snigdho.howlader@bjitgroup.com>
2023-08-28 12:43:31 +0600

    Added express JS code

--- app.js
+++ app.js
@@ -2,27 +2,27 @@ const express = require("express");
 const app = express();
 const { success } = require("./util/common");
 
-app.use(express.json());
-app.use(express.text());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json()); // Parses data as JSON
+app.use(express.text()); // Parses data as text
+app.use(express.urlencoded({ extended: true })); // Parses data as urlencoded
 
 app.get("/products", (req, res) => {
-  // console.log(req.body);
-  return res.status(200).send(success("Request successfully received", { data: { text: 123 } }));
+  return res.status(200).send({ message: "Successfully received get request" });
 });
 
-app.get("/school/detail/:course/:chapter", (req, res) => {
-  const { id, name } = req.params;
-  console.log(req.params);
-  return res.status(200).send({ message: "GET request successful" });
+app.get("/products/:category/:productId", (req, res) => {
+  const { category, productId } = req.params;
+  return res
+    .status(200)
+    .send({ message: "GET request successful", data: { category: category, productId: productId } });
 });
 
 app.post("/products", (req, res) => {
   console.log(req.body);
-  return res.status(200).send({ message: "POST request successful" });
-  // return res.statusCode(200);
+  return res.status(200).send({ message: "POST request successful", data: req.body });
 });
 
+// Route to handle all other invalid requests
 app.use((req, res) => {
   return res.status(400).send({ message: "Not found" });
 });
