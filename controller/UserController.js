db5b14be36cd8c4cec22fe862699519a3b22af91
Snigdho Dip Howlader <snigdho.howlader@bjitgroup.com>
2023-08-31 12:08:45 +0600

    Added constants for http status

--- controller/UserController.js
+++ controller/UserController.js
@@ -1,23 +1,36 @@
 const { validationResult } = require("express-validator");
 const { success, failure } = require("../util/common");
 const UserModel = require("../model/User");
+const HTTP_STATUS = require("../constants/statusCodes");
 
 class UserController {
   async getAll(req, res) {
     try {
-      // Write your code here
+      const users = await UserModel.find({});
+      if (users.length > 0) {
+        return res
+          .status(HTTP_STATUS.OK)
+          .send(success("Successfully received all users", { result: users, total: users.length }));
+      }
+      return res.status(HTTP_STATUS.OK).send(success("No users were found"));
     } catch (error) {
       console.log(error);
-      return res.status(500).send(failure("Internal server error"));
+      return res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).send(failure("Internal server error"));
     }
   }
 
   async getOneById(req, res) {
     try {
-      // Write your code here
+      const { id } = req.params;
+      const user = await UserModel.findById({ _id: id });
+      if (user) {
+        return res.status(HTTP_STATUS.OK).send(success("Successfully received the user", user));
+      } else {
+        return res.status(HTTP_STATUS.OK).send(failure("Failed to received the user"));
+      }
     } catch (error) {
       console.log(error);
-      return res.status(500).send(failure("Internal server error"));
+      return res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).send(failure("Internal server error"));
     }
   }
 
@@ -25,7 +38,7 @@ class UserController {
     try {
       const validation = validationResult(req).array();
       if (validation.length > 0) {
-        return res.status(200).send(failure("Failed to add the user", validation));
+        return res.status(HTTP_STATUS.OK).send(failure("Failed to add the user", validation));
       }
       const { name, rank, email, status } = req.body;
       const user = new UserModel({ name: name, rank: rank, email: email, status: status });
@@ -33,15 +46,15 @@ class UserController {
       await user
         .save()
         .then((data) => {
-          return res.status(200).send(success("Successfully added the user", data));
+          return res.status(HTTP_STATUS.OK).send(success("Successfully added the user", data));
         })
         .catch((err) => {
           console.log(err);
-          return res.status(200).send(failure("Failed to add the user"));
+          return res.status(HTTP_STATUS.UNPROCESSABLE_ENTITY).send(failure("Failed to add the user"));
         });
     } catch (error) {
       console.log(error);
-      return res.status(500).send(failure("Internal server error"));
+      return res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).send(failure("Internal server error"));
     }
   }
 }
