68ac64483c351c3d31cd78780c1efa7520e7cd7a
Snigdho Dip Howlader <snigdho.howlader@bjitgroup.com>
2023-09-01 20:38:26 +0600

    Adding code for transaction and population

--- controller/UserController.js
+++ controller/UserController.js
@@ -1,49 +1,92 @@
 const { validationResult } = require("express-validator");
 const { success, failure } = require("../util/common");
 const UserModel = require("../model/User");
+const HTTP_STATUS = require("../constants/statusCodes");
 
 class UserController {
-  async getAll(req, res) {
-    try {
-      // Write your code here
-    } catch (error) {
-      console.log(error);
-      return res.status(500).send(failure("Internal server error"));
+    async getAll(req, res) {
+        try {
+            const users = await UserModel.find({});
+            if (users.length > 0) {
+                return res.status(HTTP_STATUS.OK).send(
+                    success("Successfully received all users", {
+                        result: users,
+                        total: users.length,
+                    })
+                );
+            }
+            return res.status(HTTP_STATUS.OK).send(success("No users were found"));
+        } catch (error) {
+            console.log(error);
+            return res
+                .status(HTTP_STATUS.INTERNAL_SERVER_ERROR)
+                .send(failure("Internal server error"));
+        }
     }
-  }
 
-  async getOneById(req, res) {
-    try {
-      // Write your code here
-    } catch (error) {
-      console.log(error);
-      return res.status(500).send(failure("Internal server error"));
+    async getOneById(req, res) {
+        try {
+            const { id } = req.params;
+            const user = await UserModel.findById({ _id: id });
+            if (user) {
+                return res
+                    .status(HTTP_STATUS.OK)
+                    .send(success("Successfully received the user", user));
+            } else {
+                return res.status(HTTP_STATUS.OK).send(failure("User does not exist"));
+            }
+        } catch (error) {
+            console.log(error);
+            return res
+                .status(HTTP_STATUS.INTERNAL_SERVER_ERROR)
+                .send(failure("Internal server error"));
+        }
     }
-  }
 
-  async create(req, res) {
-    try {
-      const validation = validationResult(req).array();
-      if (validation.length > 0) {
-        return res.status(200).send(failure("Failed to add the user", validation));
-      }
-      const { name, rank, email, status } = req.body;
-      const user = new UserModel({ name: name, rank: rank, email: email, status: status });
-      // Using the then and catch to handle separate responses on success and failure
-      await user
-        .save()
-        .then((data) => {
-          return res.status(200).send(success("Successfully added the user", data));
-        })
-        .catch((err) => {
-          console.log(err);
-          return res.status(200).send(failure("Failed to add the user"));
-        });
-    } catch (error) {
-      console.log(error);
-      return res.status(500).send(failure("Internal server error"));
+    async create(req, res) {
+        try {
+            const validation = validationResult(req).array();
+            if (validation.length > 0) {
+                return res
+                    .status(HTTP_STATUS.OK)
+                    .send(failure("Failed to add the user", validation));
+            }
+            const { name, rank, email, address, role } = req.body;
+
+            const emailCheck = await UserModel.findOne({ email: email });
+            if (emailCheck) {
+                return res
+                    .status(HTTP_STATUS.UNPROCESSABLE_ENTITY)
+                    .send(failure("User with email already exists"));
+            }
+            const user = await UserModel.create({
+                name: name,
+                rank: rank,
+                email: email,
+                role: role,
+                address: {
+                    house: address.house,
+                    road: address.road,
+                    area: address.area,
+                    city: address.city,
+                    country: address.country,
+                },
+            });
+            if (user) {
+                return res
+                    .status(HTTP_STATUS.OK)
+                    .send(success("Successfully added the user", user));
+            }
+            return res
+                .status(HTTP_STATUS.UNPROCESSABLE_ENTITY)
+                .send(failure("Failed to add the user"));
+        } catch (error) {
+            console.log(error);
+            return res
+                .status(HTTP_STATUS.INTERNAL_SERVER_ERROR)
+                .send(failure("Internal server error"));
+        }
     }
-  }
 }
 
 module.exports = new UserController();
