bbf22e9cd551b8af91e2a25d255d2285d52af870
Snigdho Dip Howlader <snigdho.howlader@bjitgroup.com>
2023-09-05 17:02:40 +0600

    Updated response

--- controller/ProductController.js
+++ controller/ProductController.js
@@ -10,7 +10,9 @@ class Product {
             if (allProducts.length === 0) {
                 return res.status(HTTP_STATUS.NOT_FOUND).send(failure("No products were found"));
             }
-            return res.status(HTTP_STATUS.OK).send(success("Successfully got all products", allProducts));
+            return res
+                .status(HTTP_STATUS.OK)
+                .send(success("Successfully got all products", { products: allProducts, total: allProducts.length }));
         } catch (error) {
             console.log(error);
             return res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).send(failure("Internal server error"));
